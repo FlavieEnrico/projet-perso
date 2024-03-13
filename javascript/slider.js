d1193c9232b5d8857cfd83881f709380db7b1d1f
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2024-03-13 23:12:57 +0100

    what is in challenge

--- javascript/slider.js
+++ javascript/slider.js
@@ -12,12 +12,14 @@ const sliderStart = document.querySelector(".slider--btn")
 let slideCounter = 0
 
 const startSlider = () => {
+  console.log(challenges[0])
   sliderTitle.innerHTML = challenges[0].title
   sliderText.innerHTML = challenges[0].text
   }
 
   btnRight.addEventListener("click", function() {
     if (slideCounter === challenges.length - 1) {
+      console.log(challenges[0])
       sliderTitle.innerHTML = challenges[0].title
       sliderText.innerHTML = challenges[0].text
       slideCounter = -1
@@ -27,6 +29,7 @@ const startSlider = () => {
         slideContainer.classList.remove("fadeIn")
       }, 1000)
     } else {
+      console.log(challenges[slideCounter + 1])
       sliderTitle.innerHTML = challenges[slideCounter + 1].title
       sliderText.innerHTML = challenges[slideCounter + 1].text
       slideCounter++
