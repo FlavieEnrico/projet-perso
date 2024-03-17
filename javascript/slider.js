132ff953fc5bf7801bc88cd34ae50bf47b2d6bb5
FlavieEnrico <flavie.enrico.74@gmail.com>
2024-03-17 21:51:16 +0100

    :sob:

--- javascript/slider.js
+++ javascript/slider.js
@@ -25,7 +25,6 @@ if (btnRight) {
       sliderTitle.innerHTML = challenges[slideCounter + 1].title
       sliderText.innerHTML = challenges[slideCounter + 1].text
       sliderIllu.src = challenges[slideCounter + 1].illustration
-      console.log(sliderIllu.src)
       slideCounter++
       slideContainer.classList.add("fadeIn")
       setTimeout(() => {
